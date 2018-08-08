2cd2e724d781f81fca1a337c62fda03ae52dd09b
jof <dorianjof@gmail.com>
2018-08-08 04:18:46 +0100

    my second commit

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int factorial(int n);
+{
+int factorial(n);
+if(n==0){
+return 1;
+}
+else{
+return(n*factorial(n-1));
+}
+  int main()
+{
+printf("the factrorial is %d",factorial(n));
+  return o;
+}
