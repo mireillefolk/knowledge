73326e5f5ffd6ca29854029d5eb295cf662ca0ca
jof <dorianjof@gmail.com>
2018-08-08 03:13:31 +0100

    my first commit

---
+++ sum_array1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int sumarray(int arr[],int n);
+int main()
+{
+int myarray[]={1,2,3,4,5,6};
+int arraysize= sizeof(myarray[],array size);
+int result = sumarray(myarray[],array size);
+printf("%d\n",result);
+return 0;
+}
+int sumarray(int arr[],int n);
+int sum = 0;
+int i;
+for(i=0;i<n;i++);
+{
+sum = sum+arr[i];
+}
+return sum;
+}
